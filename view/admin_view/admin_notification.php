e210896f4462f83e4b58ebacd59bdb37bacbdd98
Jhon Mickelle Agumboy <32501053+jmickelle@users.noreply.github.com>
2018-11-06 00:29:08 -0800

    Update view/admin_view/admin_notification.php

--- view/admin_view/admin_notification.php
+++ view/admin_view/admin_notification.php
@@ -1,41 +1,13 @@
-<script type="text/javascript">
-var IDLE_TIMEOUT = 600; //seconds
-var _idleSecondsCounter = 0;
-document.onclick = function() {
-_idleSecondsCounter = 0;
-};
-document.onmousemove = function() {
-_idleSecondsCounter = 0;
-};
-document.onkeypress = function() {
-_idleSecondsCounter = 0;
-};
-window.setInterval(CheckIdleTime, 1000);
-function CheckIdleTime() {
-_idleSecondsCounter++;
-var oPanel = document.getElementById("SecondsUntilExpire");
-if (oPanel)
-oPanel.innerHTML = (IDLE_TIMEOUT - _idleSecondsCounter) + "";
-if (_idleSecondsCounter >= IDLE_TIMEOUT) {
-//alert("Time expired!");
-document.location.href = "../php/connection/logout.php";
-}
-}
-</script>
-
-<?php
-session_start();
-
-require "{$_SERVER['DOCUMENT_ROOT']}/php/connection/db_connection.php";
-
-if(!isset($_SESSION["userid"])) {
-  header("Location: index.php");
-exit();
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
+<?php
+    include '../../php/controller.php';
+    Login();
+    if(!isset($_SESSION["user"])) {
+        header("Location: ../../index.php");
+    }
+    Logout();
+?>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -45,14 +17,14 @@ exit();
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- Our Custom CSS -->
-    <link rel="stylesheet" href="general.css">
+    <link rel="stylesheet" href="../../public/css/style.css">
     <!-- <link rel="stylesheet" href="styleIndex.css"> -->
 </head>
 <body>
     <!-- Top Navigation -->
 	<nav class="navbar navbar-default navbar-fixed-top" class="col-lg-12 col-md-12 col-sm-12" style="background-color: #fffafa;">
 		<div class="navbar-header">
-			<img class="nav-logo" src="icons/sky_luster.png">
+			<img class="nav-logo" src="../../public/images/icons/sky_luster.png">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
@@ -73,33 +45,11 @@ exit();
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#" style="padding-right: 28px;">
                         <span class="glyphicon glyphicon-bell"></span>
                         <span class="label label-pill label-warning count" style="border-radius: 10px;">
-                        <?php
-                            $query = $db->prepare("SELECT user,hostname,iMonitor_Status FROM tbl_log WHERE iMonitor_Status = 'End Task' AND user != 'Administrator' ");
-                            $query->execute();
-                            $query->setFetchMode(PDO::FETCH_ASSOC);
-                            $countdown = 0;
-                            while ($row = $query->fetch()) {
-                                $countdown++;
-                            }
-                            echo  $countdown;
-                        ?>
+                        <?php notifCount(); ?>
                         </span>
                     </a>
                     <ul class="dropdown-menu">
-                        <?php 
-                            $query = $db->prepare("SELECT user,hostname,iMonitor_Status FROM tbl_log WHERE iMonitor_Status = 'End Task' AND user != 'Administrator' LIMIT 5 ");
-                            $query->execute();
-                            $query->setFetchMode(PDO::FETCH_ASSOC);
-                            while ($row = $query->fetch()) {
-                                echo '
-                                <li>
-                                    <a href="#"><strong>'.$row['hostname'].'</strong><br>
-                                    <small><em>'.$row['iMonitor_Status'].'</em></small></a>
-                                </li>
-                                <li class="divider"></li>
-                                ';
-                            }
-                        ?>
+                    <?php notifDisplay(); ?>
                         <li>
                             <a href=""><small>Show all notifications</small></a>
                         </li>
@@ -110,17 +60,7 @@ exit();
                 <!-- User Dropdown -->
 	            <li class="dropdown" style="padding-left: 5px;">
 	            	<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" style="padding-right: 30px;"><i class="glyphicon glyphicon-user"></i>
-                    
-                    <?php
-                        $query = $db->prepare("SELECT name FROM tbl_user WHERE userid=:userid");
-                        $query->bindValue(':userid', $_SESSION['userid'], PDO::PARAM_STR);
-                        $query->execute();
-                        $query->setFetchMode(PDO::FETCH_ASSOC);
-         
-                        while ($row = $query->fetch()) {
-                        echo 'Welcome: ' . $row['name'];
-                        }
-                    ?>
+                    <?php displayName(); ?>
 	                </a>
 	            	<ul class="dropdown-menu" role="menu">
 	            		<li class="dropdown-header"><i class="glyphicon glyphicon-cog"></i><b> Settings</b></li>
@@ -205,18 +145,18 @@ exit();
                                     </thead>
                                     <tbody>
                                         <?php
-                                            $query = $db->prepare("SELECT user,hostname,iMonitor_Status,connection_status,branch,scan_time FROM tbl_log WHERE iMonitor_Status = 'End Task' AND user != 'Administrator' ");
-                                            $query->execute();
-                                            $query->setFetchMode(PDO::FETCH_ASSOC);
-                                            while ($row = $query->fetch()) {
-                                                echo '
-                                                    <tr>
-                                                        <td>iMonitor : '.$row['iMonitor_Status'].'<br/> Port Connection:'.$row['connection_status'].'<br/> Scan Time:'.$row['scan_time'].'</td>
-                                                        <td>Hostname: '.$row['hostname'].' <br> User: '.$row['user'].'<br/> Building : '.$row['branch'].'</td>
-                                                        <td>'.$row['scan_time'].'</td>
-                                                    </tr>
-                                                ';
-                                            }
+                                            // $query = $db->prepare("SELECT user,hostname,iMonitor_Status,connection_status,branch,scan_time FROM tbl_log WHERE iMonitor_Status = 'End Task' AND user != 'Administrator' ");
+                                            // $query->execute();
+                                            // $query->setFetchMode(PDO::FETCH_ASSOC);
+                                            // while ($row = $query->fetch()) {
+                                            //     echo '
+                                            //         <tr>
+                                            //             <td>iMonitor : '.$row['iMonitor_Status'].'<br/> Port Connection:'.$row['connection_status'].'<br/> Scan Time:'.$row['scan_time'].'</td>
+                                            //             <td>Hostname: '.$row['hostname'].' <br> User: '.$row['user'].'<br/> Building : '.$row['branch'].'</td>
+                                            //             <td>'.$row['scan_time'].'</td>
+                                            //         </tr>
+                                            //     ';
+                                            // }
                                         ?>
                                     </tbody>
                                 </table>
