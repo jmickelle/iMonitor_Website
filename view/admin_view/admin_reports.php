61f403ce578e64511cd7ec34e1e5ed9b2da180cf
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-07 08:32:38 +0800

    try

--- view/admin_view/admin_reports.php
+++ view/admin_view/admin_reports.php
@@ -135,7 +135,7 @@
                     <div class="panel-heading">
                         <ul class="nav nav-tabs">
                             <li class="active"><a href="#tab0default" data-toggle="tab" style="padding-right:35px;">Computer Logs</a></li>
-                            <li><a href="#tab2default" data-toggle="tab" style="padding-right:35px;">Computer List</a></li>
+                            <li><a href="#tab2default" data-toggle="tab" style="padding-right:35px;">History</a></li>
                         </ul>
                     </div>
                     <div class="panel-body">
@@ -149,27 +149,47 @@
                                         </select> 
                                     </div>
                                     <div class="col-md-6" style="padding-top:15px;">
-                                        <select name="dub_dept" id="sub_dept" class="form-control" disabled>
-                                            <option value="" selected>--All sub department</option>
-                                                <?php listDepartment();?>
-                                        </select>
+                                        <input type="hidden" class="form-control">
                                     </div>
                                     <div class="col-md-6"><br>
                                         <input type="text" id="user" name="user" class="form-control" placeholder="Search for user... ">
                                     </div>
+                                </div>
+                                <div class="row">
                                     <div class="col-md-6">
-                                    <br>
-                                        <input type="button" id="reset" name="clear" value="Clear" class="btn btn-default">
-                                        <input type="button" id="btnExport" name="btnExport" value="Export to Excel" class="btn btn-success" onclick="fnExcelReport();">
-                                            <!--<input type="button" name="btnExport_PDF" id="btnExport_PDF" value="PDF" class="btn btn-danger" onclick="">-->
-                                        <input type="button" id="print" name="print" value="Print" class="btn btn-danger" onclick="javascript:printDiv('printablediv')" />
+                                        <br>
+                                            <input type="button" id="reset" name="clear" value="Clear" class="btn btn-default">
+                                            <input type="button" id="btnExport" name="btnExport" value="Export to Excel" class="btn btn-success" onclick="fnExcelReport();">
+                                                <!--<input type="button" name="btnExport_PDF" id="btnExport_PDF" value="PDF" class="btn btn-danger" onclick="">-->
+                                            <input type="button" id="print" name="print" value="Print" class="btn btn-danger" onclick="javascript:printDiv('printablediv')" />
                                     </div>
-                                    <iframe id="txtArea1" style="display:none"></iframe>
-                                    <div style="clear:both; padding:15px;"></div>
-                                    <div class="table-responsive"  style="overflow-x:auto; margin-left:15px;" id="tb_div">
-                                        <table class="table table-bordered" style="background: #ffffff;" id="comp_logs">
-                                            <thead>
-                                                <tr>
+                                </div>
+                                <iframe id="txtArea1" style="display:none"></iframe>
+                                <div style="clear:both; padding:15px;"></div>
+                                <div class="table-responsive"  style="overflow-x:auto; margin-left:15px;" id="tb_div">
+                                    <table class="table table-bordered" style="background: #ffffff;" id="comp_logs">
+                                        <thead>
+                                            <tr>
+                                                <th>No.</th>
+                                                <th>User</th>
+                                                <th>Computer Name</th>
+                                                <th>Domain</th>
+                                                <th>IP Address</th>
+                                                <th>Date Modified</th>
+                                                <th>iMonitor Status</th>
+                                                <th>Services Not Found</th>
+                                                <th>SysSetting File</th>
+                                                <th>Server IP</th>
+                                                <th>Connection Status</th>
+                                                <th>Branch</th>
+                                                <th>Scan Time</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
                                                     <th>No.</th>
                                                     <th>User</th>
                                                     <th>Computer Name</th>
@@ -183,13 +203,18 @@
                                                     <th>Connection Status</th>
                                                     <th>Branch</th>
                                                     <th>Scan Time</th>
-                                                </tr>
-                                            </thead>
-                                    </div>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
                                 </div>
+                            </div>
 
-            </div>
-            <div class="col-lg-12">
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                        
+                    </div>
+            <!-- <div class="col-lg-12">
                 <div class="panel with-nav-tabs panel-default" style="width: 103.5%; position: relative;  margin-left: -30px;">
                     <div class="panel-heading">
                         <ul class="nav nav-tabs">
@@ -221,7 +246,7 @@
                                             </tr>
                                         </thead>
                                         <tbody id = "load_data">
-                                            <?php
+                                            ?php
                                                 $sql = "select user, domain_name, hostname, ip_address, ip_date_modified,
                                                 iMonitor_Status, services, sysSetting_File, serverIP, connection_status, branch, scan_time from tbl_log 
                                                 WHERE user != 'Administrator' ";
@@ -233,21 +258,21 @@
                                                     //while($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
                                             ?>
                                             <tr> 
-                                                <td><?php echo $count++ ?></td>
-                                                <td><?php echo $row['user']; ?></td>
-                                                <td><?php echo $row['hostname']; ?></td>
-                                                <td><?php echo $row['domain_name']; ?></td>
-                                                <td><?php echo $row['ip_address']; ?></td>
-                                                <td><?php echo $row['ip_date_modified']; ?></td>
-                                                <td><?php echo $row['iMonitor_Status']; ?></td>
-                                                <td><?php echo $row['services']; ?></td>
-                                                <td><?php echo $row['sysSetting_File']; ?></td>
-                                                <td><?php echo $row['serverIP']; ?></td>
-                                                <td><?php echo $row['connection_status']?></td>
-                                                <td><?php echo $row['branch']; ?></td>
-                                                <td><?php echo date("M-d-Y h:m", strtotime($row['scan_time']))?></td>
+                                                <td>?php echo $count++ ?></td>
+                                                <td>?php echo $row['user']; ?></td>
+                                                <td>?php echo $row['hostname']; ?></td>
+                                                <td>?php echo $row['domain_name']; ?></td>
+                                                <td>?php echo $row['ip_address']; ?></td>
+                                                <td>?php echo $row['ip_date_modified']; ?></td>
+                                                <td>?php echo $row['iMonitor_Status']; ?></td>
+                                                <td>?php echo $row['services']; ?></td>
+                                                <td>?php echo $row['sysSetting_File']; ?></td>
+                                                <td>?php echo $row['serverIP']; ?></td>
+                                                <td>?php echo $row['connection_status']?></td>
+                                                <td>?php echo $row['branch']; ?></td>
+                                                <td>?php echo date("M-d-Y h:m", strtotime($row['scan_time']))?></td>
                                             </tr>
-                                            <?php } ?>
+                                            ?php } ?>
                                         </tbody>
                                     </table>
                                 </div>
@@ -256,7 +281,7 @@
                                 <div class="col-md-6" style="padding-top:15px;">
                                     <select name="department" id="department" class="form-control">
                                         <option value="All" selected>--All department--</option>
-                                        <?php     
+                                        ?php     
               								    $sql = "select DISTINCT branch_name from tbl_department ORDER BY branch_name ASC";
               								    $stmt = $db->prepare($sql);
               								    $stmt->execute();
@@ -290,8 +315,8 @@
                                 <br>
                                     <input type="button" id="resetl" name="clear" value="Clear" class="btn btn-default">
                                     <input type="button" id="btnExport2" name="btnExport2" value="Export to Exel" class="btn btn-success" onclick="fnExcelReport2();">
-                                        <!--<input type="button" name="btn_search" id=btn_search value="PDF" class="btn btn-danger" onclick="">-->
-                                    <input type="button" id="print2" name="print2" value="Print" class="btn btn-danger" onclick="javascript:printDiv2('printable')" />
+                                        <input type="button" name="btn_search" id=btn_search value="PDF" class="btn btn-danger" onclick="">-->
+                                    <!-- <input type="button" id="print2" name="print2" value="Print" class="btn btn-danger" onclick="javascript:printDiv2('printable')" />
                                 </div>
                                 <iframe id="txtArea1" style="display:none"></iframe>
                                 <div style="clear:both; padding:15px;"></div>
@@ -308,7 +333,7 @@
                                             </tr>
                                         <thead>
                                         <tbody id = "load_datal">
-                                            <?php
+                                            ?php
                                                 $sql = "select user,hostname, ip_address,iMonitor_Status,connection_status from tbl_log WHERE user != 'Administrator' ";
                                                 $stmt = $db->prepare($sql);
                                                 $stmt->execute();
@@ -317,19 +342,19 @@
                                                 //while($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
                                             ?>
                                             <tr>
-                                                <td><?php echo $count++ ?></td>
-                                                <td><?php echo $row['hostname']; ?></td>
-                                                <td><?php echo $row['ip_address']; ?></td>
-                                                <td><?php echo $row['iMonitor_Status']; ?></td>
-                                                <td><?php echo $row['connection_status']; ?></td>
-                                                <td><?php
+                                                <td>?php echo $count++ ?></td>
+                                                <td>?php echo $row['hostname']; ?></td>
+                                                <td>?php echo $row['ip_address']; ?></td>
+                                                <td>?php echo $row['iMonitor_Status']; ?></td>
+                                                <td>?php echo $row['connection_status']; ?></td>
+                                                <td>?php
                                                     if($row['iMonitor_Status'] == 'Running' AND $row['connection_status'] == 'ESTABLISHED')
                                                         echo 'Active';
                                                     else
                                                         echo "Inactive";
                                                 ?></td>
                                             </tr>
-                                            <?php } ?>
+                                            ?php } ?>
                                         </tbody>
                                     </table>
                                 </div>
@@ -337,9 +362,9 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
+                                                </div> -->
+        <!-- </div>
+    </div> -->
 
    <script>
 
