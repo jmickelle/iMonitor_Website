8e1d392cc0f5ec1a81bbc671509f0c976d5eac0d
Cams22 <42762357+Cams22@users.noreply.github.com>
2018-11-07 09:45:19 +0800

    Update view/admin_view/admin_reports.php

--- view/admin_view/admin_reports.php
+++ view/admin_view/admin_reports.php
@@ -151,18 +151,17 @@
                                     <div class="col-md-6" style="padding-top:15px;">
                                         <input type="hidden" class="form-control">
                                     </div>
+                                </div>
+                                <div class="row">
                                     <div class="col-md-6"><br>
                                         <input type="text" id="user" name="user" class="form-control" placeholder="Search for user... ">
                                     </div>
-                                </div>
-                                <div class="row">
                                     <div class="col-md-6">
-                                        <br>
-                                            <input type="button" id="search" name="search" value="Search" class="btn btn-primary">
-                                            <input type="button" id="reset" name="clear" value="Clear" class="btn btn-default">
-                                            <input type="button" id="btnExport" name="btnExport" value="Export to Excel" class="btn btn-success" onclick="fnExcelReport();">
+                                        <input type="button" id="search" name="search" value="Search" class="btn btn-primary">
+                                        <input type="button" id="reset" name="clear" value="Clear" class="btn btn-default">
+                                        <input type="button" id="btnExport" name="btnExport" value="Export to Excel" class="btn btn-success" onclick="fnExcelReport();">
                                                 <!--<input type="button" name="btnExport_PDF" id="btnExport_PDF" value="PDF" class="btn btn-danger" onclick="">-->
-                                            <input type="button" id="print" name="print" value="Print" class="btn btn-danger" onclick="javascript:printDiv('printablediv')" />
+                                        <input type="button" id="print" name="print" value="Print" class="btn btn-danger" onclick="javascript:printDiv('printablediv')" />
                                     </div>
                                 </div>
                                 <iframe id="txtArea1" style="display:none"></iframe>
